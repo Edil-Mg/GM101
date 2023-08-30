1eb4afdd7c904b56903a012af5bb098a4ce22c51
Edil-Mg <135086322+Edil-Mg@users.noreply.github.com>
2023-08-29 23:02:57 -0400

    nav bar added

--- gitPractice1/my-react-app/src/App.js
+++ gitPractice1/my-react-app/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-import ObjectShots
- from './Components/object-shots/ObjectShots';
+import ObjectShots from './Components/object-shots/ObjectShots';
+import NavBar from './Components/navbar/NavBar.js';
+
+
 function App() {
   return (
     <div className="App">
+    <NavBar />
     <ObjectShots />
     </div>
   );
---
+++ gitPractice1/my-react-app/src/Components/navbar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./navbar.css"
+const NavBar = () => {
+  return (
+		<div className='nav'>
+			<nav>
+				<a href="#">Home</a>
+				<a href="#">About</a>
+				<a href="#">Services</a>
+				<a href="#">Contact</a>
+			</nav>
+		</div>
+	);
+}
+
+export default NavBar
